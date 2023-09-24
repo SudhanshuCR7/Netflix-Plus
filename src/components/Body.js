34301e575ae2003cd254f00b338470894453690c
Sudhanshoo <Sudhanshu17cr7@gmail.com>
2023-09-24 15:35:45 +0530

    file

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
+import Login from './Login'
 
 const Body = () => {
   return (
     <>
      <Header/>
      <Body/>
+     <Login/>
     </>
    
 
