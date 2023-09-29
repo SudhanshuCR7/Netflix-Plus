2774f97975ef7d6e45d9115cf8b8868109a7155f
Sudhanshoo <Sudhanshu17cr7@gmail.com>
2023-09-29 09:03:19 +0530

    login page added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <Header/>
+    <img src='https://assets.nflxext.com/ffe/siteui/vlv3/9db4a880-3034-4e98-bdea-5d983e86bf52/b5953637-091d-4e02-9754-2bfadc8a8f7c/IN-en-20230925-popsignuptwoweeks-perspective_alpha_website_large.jpg'
+    alt='background'/>
+    </>
+    
   )
 }
 
