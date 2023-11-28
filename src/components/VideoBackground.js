87453c413ac8b9bd3779fe9c20856506b3c97daf
Sudhanshoo <Sudhanshu17cr7@gmail.com>
2023-11-29 01:56:39 +0530

    video-background-video-embed

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { API_OPTIONS } from '../utils/constant'
 
-const VideoBackground = () => {
+const VideoBackground = ({id}) => {
+  const [trailerId, setTrailerId] =useState(null);
+
+  const movieTrailer = async () => {
+     const data  = await fetch('https://api.themoviedb.org/3/movie/'+id+'/videos?language=en-US', API_OPTIONS)
+     const json = await data.json();
+
+     const trailer = json.results.filter((v) => v.type === "Trailer")
+     const mainTrailer = trailer[0];
+     setTrailerId(mainTrailer.key)
+  }
+
+  useEffect(() => {
+   movieTrailer();
+  }, [])
+
   return (
-    <div>VideoBackground</div>
-  )
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={'https://www.youtube.com/embed/'+ trailerId }
+        title="YouTube video player"
+       
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        
+      ></iframe>
+    </div>
+  );
 }
 
 export default VideoBackground
